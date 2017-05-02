f041824f465e6918ee14981f861491e326eaacb7
Sterling Chin <sterlingchin@Sterlings-MacBook-Pro-2.local>
2017-05-02 11:28:32 -0600

    adding cookies next

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { addReminder, deleteReminder } from '../actions';
+import moment from 'moment';
 
 
 class App extends Component {
@@ -8,11 +9,13 @@ class App extends Component {
         super(props);
         this.state = {
             text: '',
+            dueDate: ''
         }
     }
 
     addReminder(){
-        this.props.addReminder(this.state.text)
+        console.log('this.state.dueDate', this.state.dueDate)
+        this.props.addReminder(this.state.text, this.state.dueDate)
     }
 
     deleteReminder(id){
@@ -27,7 +30,10 @@ renderReminders(){
                 reminders.map(reminder => {
                     return(
                     <li key={reminder.id} className='list-group-item'>
-                        <div className='list-item'>{reminder.text}</div>
+                        <div className='list-item'>
+                            <div>{reminder.text}</div>
+                            <div><em>{moment(new Date(reminder.dueDate)).fromNow()}</em></div>
+                        </div>
                         <div className='list-item delete-button' onClick={()=>this.deleteReminder(reminder.id)}>&#x2715;</div>
                     </li>
 
@@ -48,6 +54,7 @@ renderReminders(){
             <div className='form-inline reminder-form'>
                 <div className='form-group'>
                     <input className='form-control' placeholder='I have to...' onChange={event => this.setState({text: event.target.value})}/>
+                    <input className='form-control' type='datetime-local' onChange={event => this.setState({dueDate: event.target.value})}/>
                 </div>
                 <button type='button' className='btn btn-success' onClick={()=> this.addReminder()}>Add Reminder</button>
             </div>
